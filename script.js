007154692a6a79476635bd212af480aadd4fd9d0
/home/fullpwn <57821130+fullpwn@users.noreply.github.com>
2022-06-08 22:39:02 +1000

    we back

---
+++ script.js
@@ -0,0 +1,18 @@
+debug = function(arg) {
+  if (arg == 'toggle'){
+    console.log("Toggled Debug Mode.")
+    const header = document.getElementById("greeting");
+    header.innerHTML = "Debug Mode";
+    const desc1 = document.getElementById("description1");
+    desc1.innerHTML = "You have enabled debug mode.";
+    const desc2 = document.getElementById("description2");
+    desc2.innerHTML = "Refresh to disable.";
+    const title = document.getElementById("title");
+    title.innerHTML = "Debug Mode - fullpwn";
+  }
+   
+
+    else{
+
+    }
+}
